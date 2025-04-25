f66830d68c0cf7d5d9f4e94200e1ed79b7b1d664
khanimgurbanli <zeri.q.222@gmail.com>
2025-04-25 13:18:41 +0000

    add task 4

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 972;
+		string mission = "Mission";
+                string street = "Street";
+                Console.WriteLine("{0} {1} {2}", number, mission, street);
+         }
+}
