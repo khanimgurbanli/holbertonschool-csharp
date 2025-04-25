05e177b534a89e8834bb445f19396425ea8179d7
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-25 18:13:36 +0400

    add

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,11 @@
 using System;
-
 class Program
 {
-    static void Main(string[] args)
-    {
-        double percent = .7553;
-	    double currency = 98765.4321;
-        Console.WriteLine("Percent: {0:P2}\nCurrency: {1:C2}", percent, currency);
-
-    }
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0:0.00%}", percent);
+		Console.WriteLine("Currency: {0:C}", currency);
+	}
 }
