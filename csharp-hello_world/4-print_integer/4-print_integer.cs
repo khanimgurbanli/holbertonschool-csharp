fd4065ce017e7980b50beac050e6dd6ff9555b51
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-25 17:32:22 +0400

    add

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -5,8 +5,6 @@ class Program
 	static void Main(string[] args)
 	{
 		int number = 972;
-		string mission = "Mission";
-        string street = "Street";
-        Console.WriteLine("{0} {1} {2}", number, mission, street);
+		Console.WriteLine("{0} Mission Street", number);
 	}
 }
