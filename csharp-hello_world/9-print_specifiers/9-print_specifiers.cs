f64bda5e997649966df248e80d1fc36c2b218497
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-26 16:49:21 +0400

    fix 9

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,6 @@ class Program
     {
         double percent = 0.2641;
         double currency = 12345.6789;
-        Console.WriteLine("Percent: {0:0.00%}", percent);
-        Console.WriteLine("Currency: ${0:0,00.00}", currency);
+        Console.WriteLine($"Percent: {percent:P2}\nCurrency: {currency.ToString("C2", CultureInfo.GetCultureInfo("en-US"))}");
     }
 }
