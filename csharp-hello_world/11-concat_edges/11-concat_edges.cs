c96d9a0d65b0b1bd688088ab982a1ae47c42b7fb
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-26 16:05:04 +0400

    add

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -2,11 +2,10 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        double percent = double.Parse(args[0]);
-        double currency = double.Parse(args[1]);
-        Console.WriteLine("Percent: {0:P2}", percent);
-        Console.WriteLine("Currency: ${0:N2}", currency);
-    }
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		str = str.Substring(49, 15) + ' ' + str.Substring(80, 11) + ' ' + str.Substring(88, 2) + ' ' +str.Substring(0, 2);
+		Console.WriteLine(str);
+        }
 }
