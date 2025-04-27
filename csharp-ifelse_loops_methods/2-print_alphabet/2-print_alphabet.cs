5988182eb62ec2f429075ec201cb5ee56dbc42b6
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-27 13:14:56 +0400

    add task 2-print_alphabet

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter ='a'; letter <= 'z'; letter++)
+        {
+            Console.Write(letter);
+        }
+    }
+}
