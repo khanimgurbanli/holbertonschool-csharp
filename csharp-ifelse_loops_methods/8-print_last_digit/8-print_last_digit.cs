94c7b5422d2f5f341e611a819ef34cb2f31af163
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-27 14:38:05 +0400

    add task 8

--- csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int ud = number%10;
+        if (ud < 0)
+            ud = ud * -1;
+        Console.Write(ud);
+        return ud;
+    }
+}
