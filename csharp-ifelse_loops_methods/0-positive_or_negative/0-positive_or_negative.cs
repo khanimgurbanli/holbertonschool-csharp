a8850e31cebf1542d64a6c464286d78ff618be2f
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-27 12:59:27 +0400

    add task positive-negative number

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -7,9 +7,17 @@ class Program
         Random rndm = new Random();
         int number = rndm.Next(-10, 10);
 
-       if(number >= 0)
-        Console.WriteLine($"{number} is postive");
-       else
-        Console.WriteLine($"{number} is negative");
+        if(number > 0)
+        {
+            Console.WriteLine($"{number} is positive");
+        }
+        else if(number < 0)
+        {
+            Console.WriteLine($"{number} is negative");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is zero");
+        }
     }
 }
