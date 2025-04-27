8e9ff5bea8144e75dc4ede552cfcbdb4485f39df
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-27 13:55:26 +0400

    add task 6

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<=99; i++)
+                {
+                    int d1 = i % 10;
+                    int d2 = i / 10;
+                    if(d1 != d2 && d1 > d2)
+                    {
+                        if(i!=89)
+                            Console.Write("{0:D2}, ", i);
+                        else
+                            Console.Write("{0:D2}\n", i);
+                    }
+                }
+        }
+    }
+}
