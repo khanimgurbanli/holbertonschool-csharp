36e3e904ee671c4b985d35d6e866ac845b3036c1
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-27 14:15:15 +0400

    add task 7

--- csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if(c >= 'a' && c <= 'z')
+            return true;
+        else
+            return false;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char[] letters = { 'a', 'A', 'Q', 'h', '9', 'B', 'g'};
+        int i;
+
+        for (i = 0; i < letters.Length; i++)
+        {
+            if(Character.IsLower(letters[i]))
+                Console.WriteLine("{0} is lowercase", letters[i]);
+            else
+                Console.WriteLine("{0} is uppercase", letters[i]);
+        }
+    }
+}
