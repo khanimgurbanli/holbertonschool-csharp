6ff7cfc9aedc651e077eb1300cc767c61a7b3c96
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-27 13:36:28 +0400

    _4_print_hexa

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,7 @@ class Program
     {
         for (int number = 0; number <= 98; number++)
         {
-            Console.Write($"{number} = 0x{number:x}\n");
+            Console.WriteLine($"{number} = 0x{number:x}\n");
         }
     }
 }
