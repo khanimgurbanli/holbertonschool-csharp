fd50e515ecc6677e7540ac4286ffb74bd4484c0e
Khanim Gurbanli <khanimgurbanzade@gmail.com>
2025-04-27 13:25:34 +0400

    [200~3-print_alphabt~

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter ='a'; letter <= 'z'; letter++)
+        {
+            if (letter != 'q' && letter != 'e')
+            {
+                Console.Write(letter);
+            }
+        }
+    }
+}
