14401f3415c5312b408a220aa71249f32ba7b09e
khanimgurbanli <zeri.q.222gmail.com>
2025-05-04 17:25:51 +0400

    add

--- csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -5,12 +5,12 @@ class Number
     public static int PrintLastDigit(int number)
     {
         int num = number % 10;
-        if (num < 0) {
-            num = num * -1;
-            Console.Write("{0}", num);
-            return (num);
+        if (num < 0)
+        {
+            num *= -1;
         }
         Console.Write("{0}", num);
-        return (num);
+        return num;
     }
 }
+
